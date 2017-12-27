64f2ae24804d0cbeff14debca6c92595fcc19b33
HoldYourWaffle <ravivanrooijen@live.nl>
2017-12-27 20:55:15 +0100

    No longer required on the clientside (v1.1)

--- src/main/java/info/zthings/mcmods/adl/ADL.java
+++ src/main/java/info/zthings/mcmods/adl/ADL.java
@@ -9,10 +9,10 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
-@Mod(modid = ADL.MODID, version = ADL.VERSION, dependencies="required-after:deathcounter")
+@Mod(modid = ADL.MODID, version = ADL.VERSION, dependencies="required-after:deathcounter", acceptableRemoteVersions = "*")
 public class ADL {
 	public static final String MODID = "adl";
-	public static final String VERSION = "1.0";
+	public static final String VERSION = "1.1";
 	
 	@EventHandler
 	public void init(FMLInitializationEvent event) {
@@ -22,7 +22,6 @@ public class ADL {
 	@SubscribeEvent(priority = EventPriority.LOWEST)
 	public void onDeath(LivingDeathEvent e) {
 		if (e.getEntity() instanceof EntityPlayer && !e.getEntityLiving().world.isRemote) {
-			System.out.println("A player has died and I am the server");
 			e.getEntityLiving().getServer().getCommandManager().executeCommand(e.getEntityLiving().getServer(), "dc leaderboard all");
 		}
 	}
